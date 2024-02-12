ba50908a0a5ebd9da7562144f7e1559c00401176
Umer-Islam <139012668+Umer-Islam@users.noreply.github.com>
2024-02-13 01:50:26 +0500

    components and portfolio page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -49,12 +49,12 @@ export default function Navbar() {
                     href="/projects"
                     prefetch
                     className={`${
-                      pathname === "/projects"
+                      pathname === "/portfolio"
                         ? "border-teal-500 dark:text-white h-full inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
                         : "border-transparent text-gray-500 dark:text-gray-300 dark:hover:text-white inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
                     }`}
                   >
-                    Projects
+                    Portfolio
                   </Link>
                   <Themebutton />
                 </div>
@@ -100,7 +100,7 @@ export default function Navbar() {
           </div>
 
           <Disclosure.Panel className="sm:hidden">
-            <div className="pt-2 pb-3 space-y-1">
+            <div className="pt-2  border-b-2 border-yellow-500 pb-3 space-y-1">
               <Link
                 href="/"
                 prefetch
@@ -124,15 +124,15 @@ export default function Navbar() {
                 Guestbook
               </Link>
               <Link
-                href="/projects"
+                href="/portfolio"
                 prefetch
                 className={`${
-                  pathname == "/projects"
+                  pathname == "/portfolio"
                     ? "bg-teal-50 border-teal-500 text-teal-500 block pl-3 pr-4 py-2 border-l-4 text-base font-medium dark:bg-gray-800"
                     : "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-teal-500 block pl-3 pr-4 py-2 dark:hover:bg-gray-700 border-l-4 text-base font-medium dark:text-white"
                 } `}
               >
-                Projects
+                Portfolio
               </Link>
             </div>
           </Disclosure.Panel>
