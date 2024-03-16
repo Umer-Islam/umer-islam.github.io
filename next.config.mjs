d945dee03b9cb567b310b200670399e830e57d33
Umer-Islam <139012668+Umer-Islam@users.noreply.github.com>
2024-03-16 18:00:43 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
-/
-  @type {import('next').NextConfig}
- 
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   output: 'export',
  
@@ -10,7 +10,7 @@ const nextConfig = {
   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
   // skipTrailingSlashRedirect: true,
  
-   Optional: Change the output directory `out` -> `dist`
+  // Optional: Change the output directory `out` -> `dist`
    distDir: 'dist',
 }
  
